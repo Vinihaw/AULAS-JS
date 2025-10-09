cbf25d9d5929adbca8df7e991d137c3d8d2a386b
Matheus Luz <daluzpimntelmatheus@gmail.com>
2025-10-09 11:30:29 -0300

    Aulas JS IOS T01/POA

---
+++ Aula02/exercício-operadores.js
@@ -0,0 +1,11 @@
+let nome, nota1, nota2, media = 7
+
+nome = `Vinicius`
+nota1 = 7
+nota2 = 9
+media = (nota1 + nota2) / 2
+console.log(`A média do Aluno é ${media}`);
+
+let status = true
+let curso = media >= 7 && status == true ? `foi aprovado!` : `não foi aprovado!`
+console.log(`O Aluno ${nome} ${curso}`);
