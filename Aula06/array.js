f9e020b2d64113081ef93c39d61b91778ffdfd5d
Vinihaw <vinicius.souza.haw@gmail.com>
2025-10-20 09:39:57 -0300

    Aula JS IOS T01/POA

---
+++ Aula06/array.js
@@ -0,0 +1,8 @@
+let codesix = [`Vinicius`, `Mateus`, `Vitor`, `Guilherme`, `Erick`, `Luis`]
+//                   0           1        2          3         4       5
+const cabo_solto = new Array(`Vitor`, `PH`, `Erik`, `Geovanne`, `Mateus`)
+//                              0       1      2        3          4
+console.log(codesix.length);
+console.log(cabo_solto.length);
+let nome = `Ermeson`
+console.log(nome.length);
---
+++ Aula06/metodos.js
@@ -0,0 +1,14 @@
+let nome = `Vinicius Gabriel Souza Alves`
+let decola = `AleXandre`
+console.log(nome.charAt(3));
+console.log(decola.charAt(2));
+
+
+console.log(nome.toUpperCase());
+console.log(decola.toLowerCase());
+
+console.log(`---------------`);
+console.log(nome.substring(1, 3));
+let nova_palavra = nome.split(` `)
+console.log(nova_palavra[3]);
+
